979549bd37e240ab92b36cf8b22ae06653aa7dcd
mxblk <63249012+mxblk@users.noreply.github.com>
2021-02-11 08:57:53 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var rellax1 = new Rellax('.sh_1'),
+    rellax2 = new Rellax('.sh_2'),
+    rellax3 = new Rellax('.sh_3'),
+    rellax4 = new Rellax('.sh_4'),
+    rellax5 = new Rellax('.sh_5'),
+    rellax6 = new Rellax('.sh_6'),
+    rellax7 = new Rellax('.sh_7'),
+    rellax8 = new Rellax('.sh_8'),
+    rellax9 = new Rellax('.sh_9'),
+    rellax10 = new Rellax('.sh_10'),
+    rellax11 = new Rellax('.sh_11'),
+    rellax12 = new Rellax('.sh_12'),
+    rellax13 = new Rellax('.sh_13'),
+    rellax14 = new Rellax('.sh_14'),
+    rellax15 = new Rellax('.sh_15'),
+    rellax16 = new Rellax('.sh_16')
+
+$(function(){
+
+
+
+
+});
